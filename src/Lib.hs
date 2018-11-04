7ff912e33c71f5d4e3ab150f40c4b874cde02e32
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-11-04 11:28:13 +0100

    Generators for dependent sums

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,60 +1,75 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
 import Data.Dependent.Map
 import Data.Dependent.Sum
 import Data.Functor.Identity
+import Data.GADT.Show
 import Data.GenRelativeValidity
 import Data.GenValidity
 import Test.QuickCheck
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type GGen t = Gen (GGenResult t)
 
+newtype GGenResult t = GGenResult
+    { getGGenResult :: forall b. (forall a. t a -> b) -> b
+    }
+
+class GADTGenUnchecked f where
+    gadtGenUnchecked :: GGen f
+
+class GenTagUnchecked tag f where
+    genTagUnchecked :: tag a -> Gen (f a)
+
+instance (GADTGenUnchecked tag, GenTagUnchecked tag f) =>
+         GenUnchecked (DSum tag f) where
+    genUnchecked = do
+        genResult <- gadtGenUnchecked
+        getGGenResult genResult $ \tag -> do
+            funct <- genTagUnchecked tag
+            pure $ tag :=> funct
+    shrinkUnchecked _ = []
+
+---------------
+--- EXAMPLE ---
+---------------
 data Tag a where
     AString :: Tag String
     AnInt :: Tag Int
 
-data TagBox f where
-    TagBox :: f a -> TagBox f
-
-class GADTGenUnchecked f where
-    gadtGenUnchecked :: Gen (TagBox f)
-
 instance GADTGenUnchecked Tag where
-    gadtGenUnchecked = elements [TagBox AString, TagBox AnInt]
-
-class GenTagUnchecked tag f where
-    genTagUnchecked :: tag a -> Gen (f a)
+    gadtGenUnchecked =
+        elements [GGenResult (\x -> x AString), GGenResult (\x -> x AnInt)]
 
 instance GenTagUnchecked Tag Identity where
     genTagUnchecked AString = Identity <$> genUnchecked
     genTagUnchecked AnInt = Identity <$> genUnchecked
 
-instance (GADTGenUnchecked tag, GenTagUnchecked tag f) =>
-         GenUnchecked (DSum tag f) where
-    genUnchecked = do
-        tb <- gadtGenUnchecked @tag
-        let TagBox tag = tb
-        funct <- genTagUnchecked (tag :: tag a)
-        pure $ tag :=> (funct :: f a)
-    shrinkUnchecked _ = []
--- class GenTagUnchecked tag f where
---     genTagUnchecked  :: TagBox f => Gen
---
--- instance (GenUncheckedTag tag f) => GenUnchecked (DSum tag f) where
+instance GShow Tag where
+    gshowsPrec _ AString = showString "AString"
+    gshowsPrec _ AnInt = showString "AnInt"
+
+instance ShowTag Tag Identity where
+    showTaggedPrec AString _ (Identity s) = showString s
+    showTaggedPrec AnInt i (Identity int) = showsPrec i int
+
+someFunc :: IO ()
+someFunc = sample (genUnchecked :: Gen (DSum Tag Identity))
+-- instance (GADTGenUnchecked tag, GenTagUnchecked tag f) =>
+--          GenUnchecked (DSum tag f) where
 --     genUnchecked = do
---         tag <- genUncheckedTag
---         funct <- genRelativeTag
---         pure $ tag :=> funct
+--         tb <- gadtGenUnchecked @tag
+--         let TagBox tag = tb
+--         funct <- genTagUnchecked (tag :: tag a)
+--         pure $ tag :=> (funct :: f a)
+--     shrinkUnchecked _ = []
 --
--- class GenUncheckedTag tag where
---     genUncheckedTag :: Gen (tag a)
---     genRelativeTag :: tag a -> Gen (f a)
+-- type ReadS a = String -> [(a , String)]
+-- type GReadS t = String -> [(GReadResult t, String)]
+-- newtype GReadResult t = forall b (forall a. t a -> b) - >b
